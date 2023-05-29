e3aaec6e8e89c96292e47773c2bdaea398480846
Traversode <131562036+Traversode@users.noreply.github.com>
2023-05-29 01:05:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ OPENAI_API_KEY = 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
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 
